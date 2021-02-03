66dae7073685d49d83f2f59de209ae2825541eb3
John Manack <jmnck28@gmail.com>
2021-02-03 08:40:35 -0500

    updates express server; introduces models, views, controllers

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express'),
+    router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('template', {
+        locals: {
+            title: "Homepage",
+        },
+        partials: {
+            body: "template"
+        }
+    });
+});
+
+module.exports = router;
